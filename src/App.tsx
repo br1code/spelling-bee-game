e0b640f9d5a95bf8d0871e59df610717dc89f10c
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-08-20 11:58:29 -0300

    fetching mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('/api/data.json', {
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error('Failed to fetch data' + error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1>My app</h1>
