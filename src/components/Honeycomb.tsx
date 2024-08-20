42858fb57ae90eb9ba3fe4047107b404d10669b6
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-08-20 16:21:05 -0300

    adding a button to shuffle the outer letters

--- src/components/Honeycomb.tsx
+++ src/components/Honeycomb.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Letter from './Letter';
 
 interface HoneycombProps {
@@ -12,13 +12,42 @@ const Honeycomb: FC<HoneycombProps> = ({
   outerLetters,
   validLetters,
 }) => {
+  const [randomOuterLetters, setRandomOuterLetters] =
+    useState<string[]>(outerLetters);
+
+  const onShuffleClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+
+    const randomizedLetters = [...randomOuterLetters].sort(
+      () => Math.random() - 0.5
+    );
+    setRandomOuterLetters(randomizedLetters);
+  };
+
+  const generateRandomKey = (letter: string, index: number): string => {
+    return `${letter}_${index}_${Math.random()}`;
+  };
+
   return (
-    <article className="honeycomb">
-      <Letter letter={centerLetter} isCenter={true} />
-      {outerLetters.map((letter, index) => (
-        <Letter letter={letter} isCenter={false} key={index} />
-      ))}
-    </article>
+    <>
+      <article className="honeycomb">
+        <Letter letter={centerLetter} isCenter={true} />
+        {randomOuterLetters.map((letter, index) => (
+          <Letter
+            letter={letter}
+            isCenter={false}
+            key={generateRandomKey(letter, index)}
+          />
+        ))}
+      </article>
+      <section className="buttons">
+        <button className="button" onClick={onShuffleClick}>
+          Shuffle
+        </button>
+      </section>
+    </>
   );
 };
 
